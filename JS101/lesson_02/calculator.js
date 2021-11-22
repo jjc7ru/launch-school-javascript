bcbd32eb0d3d5621274e886b3ce1f6cf0f731951
Jason Choi <jjc7ru@gmail.com>
2021-11-21 23:05:36 -0500

    initial commit

---
+++ JS101/lesson_02/calculator.js
@@ -0,0 +1,81 @@
+/* Goal: Build a command line calculate
+ * (1) Ask the user for two numbers.
+ * (2) Ask the user for the type of operation to perform:
+ *     add, subtract, multiply or divide.
+ * (3) Perform the calcuation and display the result.
+ * */
+
+// Import libraries
+const readline = require('readline-sync');
+const CONFIG = require('./config.json');
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+// Perform calculation
+function performCalculation(response) {
+  // Introduction
+  prompt(CONFIG.welcome);
+
+  // Ask the user for the first number.
+  prompt("What's the first number?");
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number1 = readline.question();
+  }
+
+  // Ask the user for the second number.
+  prompt("What's the second number?");
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number2 = readline.question();
+  }
+
+  // Ask the user for an operation to perform.
+  prompt(`What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide`);
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt("Must choose 1, 2, 3 or 4");
+    operation = readline.question();
+  }
+
+  // Perform the operation on the two numbers.
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  // Print the result to the terminal.
+  console.log(`The result is: ${output}`);
+}
+
+// Run function to perform calculation(s)
+let response = '1';
+while (response === '1') {
+  performCalculation(response);
+
+  // Asks the user to continue
+  prompt("Would you like to perform another calculation?\n1) Yes\n2) No");
+  response = readline.question();
+}
