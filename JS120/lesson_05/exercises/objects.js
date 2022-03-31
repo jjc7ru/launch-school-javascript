8683e04cc18a9081fbdd7eca493176c5c3e114f9
Jason Choi <jjc7ru@gmail.com>
2022-03-31 09:40:12 -0400

    Finish exercise problems

--- JS120/lesson_05/exercises/objects.js
+++ JS120/lesson_05/exercises/objects.js
@@ -1,4 +1,3 @@
-// @ 5:45
 // (1) The code below is expected to output the following when run:
 //     > helloVictor = createGreeter('Victor');
 //     > helloVictor.greet('morning');
@@ -93,33 +92,33 @@ console.log(objectsEqual({a: 'foo', b: undefined}, {a: 'foo', c: 1}));  // false
 //     > listCourses: Returns a list of the courses student has enrolled in.
 //     > addNote: Adds a note property to a course. Takes a code and a note as an argument. 
 //                If a note already exists, the note is appended to the existing one.
-//     > updateNote: Updates a note for a course. Updating a note replaces the existing note with the new note.
+//     > updateNote: Updates a note for a course. Updating a note replaces the 
+//                   existing note with the new note.
 //     > viewNotes: Logs the notes for all the courses. Courses without notes are not displayed.
 /*
 function createStudent(name, year) {
 	return {
-    courses: {},
+    name,
+    year,
+    courses: [],
+    codes: {},
     notes: {},
 
     info() {
-      console.log(`${name} is a ${year} year student`);
+      console.log(`${this.name} is a ${this.year} year student`);
     },
 
-    addCourse(course) {
-      this.courses[course.code] = course.name;
+    addCourse(courseObj) {
+      this.courses.push(courseObj);
+      this.codes[courseObj.code] = courseObj.name;
     },
 
     listCourses() {
-      let courses = [];
-      for (let code of Object.keys(this.courses)) {
-        let temp = {name: this.courses[code], code: code};
-        courses.push(temp);
-      }
-      console.log(courses);
+      console.log(this.courses);
     },
 
     addNote(code, notes) {
-      let subject = this.courses[code];
+      let subject = this.codes[code];
       this.notes[subject] = this.notes[subject] || [];
       this.notes[subject].push(notes);
     },
@@ -132,7 +131,7 @@ function createStudent(name, year) {
     },
     
     updateNote(code, notes) {
-      let subject = this.courses[code];
+      let subject = this.codes[code];
       this.notes[subject] = [notes];
     },
 	}
@@ -168,7 +167,8 @@ foo.viewNotes();
 //     > addStudent: Adds a student by creating a new student and adding the student to 
 //                   a collection of students. The method adds a constraint that the year 
 //                   can only be any of the following values: '1st', '2nd', '3rd', '4th', or 
-//                   '5th'. Returns a student object if year is valid otherwise it logs "Invalid Year".
+//                   '5th'. Returns a student object if year is valid otherwise it 
+//                   logs "Invalid Year".
 //     > enrollStudent: Enrolls a student in a course.
 //     > addGrade: Adds the grade of a student for a course.
 //     > getReportCard: Logs the grades of a student for all courses. If the course has 
@@ -177,40 +177,93 @@ foo.viewNotes();
 //                     student with grades are part of the course report.
 //     To test your code, use the three student objects listed below. Using the three student objects, 
 //     produce the following values from the getReportCard and courseReport methods respectively.
+
 function School() {
   return {
-    addStudent() {},
-    enrollStudent() {},
-    addGrade() {},
-    getReportCard(),
-    courseReport(),
+    students: {},
+    courses: {},
+    subjects: {},
+
+    addStudent(studentObj) {
+      // this.students = {'foo': studentObject(name, year)};
+      let VALID_YEARS = ['1st', '2nd', '3rd', '4th', '5th'];
+      if (VALID_YEARS.includes(studentObj.year)) {
+        this.students[studentObj.name] = studentObj;
+      } else {
+        throw "Invalid Year";
+      }
+    },
+
+    enrollStudent(studentObj) {
+      this.courses[studentObj.courses] = this.courses[studentObj.courses] || [];
+      this.courses[studentObj.courses].push(studentObj);
+    },
+    
+    addGrade(student, course, grade) {
+      let studentName = student.name;
+      let studentCourses = this.students[studentName].courses;
+      for (let i = 0; i < studentCourses.length; i++) {
+        if (studentCourses[i].name === course) {
+          studentCourses[i]['grade'] = grade;
+          this.subjects[course] = this.subjects[course] || [];
+          this.subjects[course].push([studentName, grade]);
+        }
+      }
+    },
+
+    getReportCard(studentObj) {
+      let studentCourses = this.students[studentObj.name].courses;
+      for (let i = 0; i < studentCourses.length; i++) {
+        let grade = studentCourses[i].grade;
+        if (!grade) {
+          grade = "In progress";
+        }
+        console.log('= ' + studentCourses[i].name + ': ' + grade);
+      }
+    },
+
+    courseReport(course) {
+      let total = 0;
+      console.log('=' + course + ' Grades=');
+      let nameAndGrades = this.subjects[course];
+      if (!nameAndGrades) {
+        console.log(undefined);
+        return
+      }
+      let length = nameAndGrades.length;
+      for (let [name, grade] of nameAndGrades) {
+        total += grade;
+        console.log(name + ': ' + grade);
+      }
+      console.log('---');
+      console.log(total / length);
+    },
   }
 }
 
 function createStudent(name, year) {
 	return {
-    courses: {},
+    name,
+    year,
+    courses: [],
+    codes: {},
     notes: {},
 
     info() {
-      console.log(`${name} is a ${year} year student`);
+      console.log(`${this.name} is a ${this.year} year student`);
     },
 
-    addCourse(course) {
-      this.courses[course.code] = course.name;
+    addCourse(courseObj) {
+      this.courses.push(courseObj);
+      this.codes[courseObj.code] = courseObj.name;
     },
 
     listCourses() {
-      let courses = [];
-      for (let code of Object.keys(this.courses)) {
-        let temp = {name: this.courses[code], code: code};
-        courses.push(temp);
-      }
-      console.log(courses);
+      console.log(this.courses);
     },
 
     addNote(code, notes) {
-      let subject = this.courses[code];
+      let subject = this.codes[code];
       this.notes[subject] = this.notes[subject] || [];
       this.notes[subject].push(notes);
     },
@@ -223,16 +276,101 @@ function createStudent(name, year) {
     },
     
     updateNote(code, notes) {
-      let subject = this.courses[code];
+      let subject = this.codes[code];
       this.notes[subject] = [notes];
     },
 	}
 }
 
+let foo = createStudent('foo', '2nd');
+foo.addCourse({ name: 'Math', code: 101 });
+foo.addCourse({ name: 'Advanced Math', code: 102 });
+foo.addCourse({ name: 'Physics', code: 202 });
 
+let bar = createStudent('bar', '1st');
+bar.addCourse({ name: 'Math', code: 101 });
 
+let qux = createStudent('qux', '2nd');
+qux.addCourse({ name: 'Math', code: 101 });
+qux.addCourse({ name: 'Advanced Math', code: 102 });
 
+let school = School();
+school.addStudent(foo);
+school.addStudent(bar);
+school.addStudent(qux);
 
+school.enrollStudent(foo);
+school.enrollStudent(bar);
+school.enrollStudent(qux);
+
+school.addGrade(foo, 'Math', 95);
+school.addGrade(foo, 'Advanced Math', 90);
+school.addGrade(bar, 'Math', 91);
+school.addGrade(qux, 'Math', 93);
+school.addGrade(bar, 'Advanced Math', 90);
+
+school.getReportCard(foo);
+school.courseReport('Math');
+school.courseReport('Advanced Math');
+school.courseReport('Physics');
+
+// Examples of created student objects with grades; methods on the objects are not shown here for brevity.
+// The following are only showing the properties that aren't methods for the three objects
+
+/*
+foo;
+{
+  name: 'foo',
+  year: '3rd',
+  courses: [
+    { name: 'Math', code: 101, grade: 95, },
+    { name: 'Advanced Math', code: 102, grade: 90, },
+    { name: 'Physics', code: 202, }
+  ],
+}
+
+bar;
+{
+  name: 'bar',
+  year: '1st',
+  courses: [
+    { name: 'Math', code: 101, grade: 91, },
+  ],
+}
+
+qux;
+{
+  name: 'qux',
+  year: '2nd',
+  courses: [
+    { name: 'Math', code: 101, grade: 93, },
+    { name: 'Advanced Math', code: 102, grade: 90, },
+   ],
+}
+
+school.getReportCard(foo);
+// = Math: 95
+// = Advanced Math: 90
+// = Physics: In progress
+
+school.courseReport('Math');
+// =Math Grades=
+// foo: 95
+// bar: 91
+// qux: 93
+// ---
+// Course Average: 93
+
+school.courseReport('Advanced Math');
+// =Advanced Math Grades=
+// foo: 90
+// qux: 90
+// ---
+// Course Average: 90
+
+school.courseReport('Physics');
+// undefined
+*/
 
 
 
