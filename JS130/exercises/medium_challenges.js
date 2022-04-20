a5d920cd6f8bc977b448e87cdcc88d224a17634e
Jason Choi <jjc7ru@gmail.com>
2022-04-20 15:41:36 -0400

    updating medium challenges and test file

--- JS130/exercises/medium_challenges.js
+++ JS130/exercises/medium_challenges.js
@@ -67,12 +67,136 @@ class Diamond {
 
 
 class Robot {
+  static ALPHA = Robot.createAlpha();
+  static NUMBERS = Robot.createNumbers();
+  static NAMES = new Set();
+  
   constructor() {
+    this.robotName = this.preprocessing();
   }
 
-  name() {};
+  preprocessing() {
+    let name = this.createRandomName();
+    while (Robot.NAMES.has(name)) {
+      name = this.createRandomName();
+    }
+    this.pushRandomName(name);
+    return name;
+  }
+
+  static createAlpha() {
+    return ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  };
+
+  static createNumbers() {
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  }
+
+  randomAlphaIndex() {
+    return Math.floor(Math.random() * (Robot.ALPHA.length - 1));
+  }
+
+  randomNumberIndex() {
+    return Math.floor(Math.random() * (Robot.NUMBERS.length - 1));
+  }
+
+  createRandomName() {
+    let out = '';
+    for (let i = 0; i < 2; i += 1) out += Robot.ALPHA[this.randomAlphaIndex()];
+    for (let i = 0; i < 3; i += 1) out += Robot.NUMBERS[this.randomNumberIndex()];
+    return out;
+  }
+
+  pushRandomName(name) {
+    Robot.NAMES.add(name);
+  }
+
+  name() {
+    return this.robotName;
+  }
+
+  reset() {
+    let robot = new Robot();
+    this.robotName = robot.robotName;
+  }
 }
+
+
+class Clock {
+  constructor() {
+    this.hour = 0;
+    this.minute = 0;
+  }
+
+  static at(hour, minute = 0) {
+    let clock = new Clock();
+
+    if (hour === 0) {
+      clock.hour = 24;
+      clock.minute = minute;
+      return clock;
+    }
+    clock.hour += hour;
+    clock.minute = minute;
+    return clock;
+  };
+
+  add(minute) {
+    this.minute += minute;
+    return this;
+  };
+
+  subtract(minute) {
+    this.minute -= minute;
+    return this;
+  }
+  
+  needsUpdate() {
+    if (this.minute < 0 || this.minute > 59) return true;
+    return false;
+  }
+
+  minutePositive() {
+    let hour = Math.floor(this.minute / 60);
+    let minute = this.minute % 60;
+    this.hour = (this.hour += hour) % 24;
+    this.minute = minute;
+  };
+
+  minuteNegative() {
+    let upperBound = 60;
+    while (upperBound < Math.abs(this.minute)) upperBound += 60;
+    let positiveMin = upperBound + this.minute + upperBound;
+    this.hour = (this.hour - (Math.floor(positiveMin / 60) % 24)) % 24;
+    this.minute = positiveMin % 60;
+  };
+
+  update() {
+    if (!this.needsUpdate()) return;
+    if (this.minute >= 0) {
+      this.minutePositive();
+    } else {
+      this.minuteNegative();
+    }
+  }
+
+  isEqual(clock) {
+    return JSON.stringify(this) === JSON.stringify(clock);
+  }
+
+  toString() {
+    this.update();
+    let [strHour, strMin] = [String(this.hour), String(this.minute)];
+    if (strHour.length !== 2) strHour = '0' + strHour;
+    if (strMin.length !== 2) strMin = '0' + strMin;
+    return strHour + ':' + strMin;
+  }
+}
+
 
 module.exports = {
   Diamond,
+  Robot,
+  Clock,
 }
--- JS130/exercises/medium_challenges.test.js
+++ JS130/exercises/medium_challenges.test.js
@@ -1,6 +1,7 @@
 const {
   Diamond,
   Robot,
+  Clock,
 } = require('./medium_challenges');
 
 
@@ -52,13 +53,13 @@ describe("Robot Name", () => {
     expect(new Robot().name()).toMatch(NAME_REGEXP);
   });
 
-  xtest("name sticks", () => {
+  test("name sticks", () => {
     let robot = new Robot();
     let name = robot.name();
     expect(robot.name()).toBe(name);
   });
 
-  xtest("different robots have different names", () => {
+  test("different robots have different names", () => {
     Math.seedrandom(DIFFERENT_ROBOT_NAME_SEED);
 
     let robot1 = new Robot();
@@ -67,7 +68,7 @@ describe("Robot Name", () => {
     expect(robot1.name()).not.toBe(robot2.name());
   });
 
-  xtest("different name when chosen name is taken", () => {
+  test("different name when chosen name is taken", () => {
     Math.seedrandom(SAME_INITIAL_ROBOT_NAME_SEED);
     let robot1 = new Robot();
     Math.seedrandom(SAME_INITIAL_ROBOT_NAME_SEED);
@@ -75,7 +76,7 @@ describe("Robot Name", () => {
     expect(robot1.name()).not.toBe(robot2.name());
   });
 
-  xtest("reset name", () => {
+  test("reset name", () => {
     Math.seedrandom(DIFFERENT_ROBOT_NAME_SEED);
 
     let robot = new Robot();
@@ -84,5 +85,83 @@ describe("Robot Name", () => {
     let name2 = robot.name();
 
     expect(name1).not.toBe(name2);
+  });
+});
+
+
+describe("Clock", () => {
+  test("on the hour", () => {
+    expect(Clock.at(8).toString()).toBe('08:00');
+    expect(Clock.at(9).toString()).toBe('09:00');
+  });
+
+  test("past the hour", () => {
+    expect(Clock.at(11, 9).toString()).toBe('11:09');
+  });
+
+  test("add a few minutes", () => {
+    let clock = Clock.at(10).add(3);
+    expect(clock.toString()).toBe('10:03');
+  });
+
+  test("subtract fifty minutes", () => {
+    let clock = Clock.at(0).subtract(50);
+    expect(clock.toString()).toBe('23:10');
+  });
+
+  test("add over an hour", () => {
+    let clock = Clock.at(10).add(61);
+    expect(clock.toString()).toBe('11:01');
+  });
+
+  test("wrap around at midnight", () => {
+    let clock = Clock.at(23, 30).add(60);
+    expect(clock.toString()).toBe('00:30');
+  });
+
+  test("add more than a day", () => {
+    let clock = Clock.at(10).add(3061);
+    expect(clock.toString()).toBe('13:01');
+  });
+
+  test("subtract a few minutes", () => {
+    let clock = Clock.at(10, 30).subtract(5);
+    expect(clock.toString()).toBe('10:25');
+  });
+
+  test("subtract minutes", () => {
+    let clock = Clock.at(10).subtract(90);
+    expect(clock.toString()).toBe('08:30');
+  });
+
+  test("equivalent clocks", () => {
+    let clock1 = Clock.at(15, 37);
+    let clock2 = Clock.at(15, 37);
+
+    expect(clock1.isEqual(clock2)).toBe(true);
+  });
+
+  test("wrap around at negative midnight", () => {
+    let clock = Clock.at(0, 30).subtract(60);
+    expect(clock.toString()).toBe('23:30');
+  });
+
+  test("subtract more than a day", () => {
+    let clock = Clock.at(10).subtract(3061);
+    expect(clock.toString()).toBe('06:59');
+  });
+
+  test("inequivalent clocks", () => {
+    let clock1 = Clock.at(15, 37);
+    let clock2 = Clock.at(15, 36);
+    let clock3 = Clock.at(14, 37);
+
+    expect(clock1.isEqual(clock2)).toBe(false);
+    expect(clock1.isEqual(clock3)).toBe(false);
+  });
+
+  test("wrap around backwards", () => {
+    let clock = Clock.at(0, 30).subtract(60);
+    expect(clock.toString()).toEqual('23:30');
   });
 });
