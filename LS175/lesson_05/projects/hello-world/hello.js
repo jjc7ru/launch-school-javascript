c1faca3c6b00703a0ded0ecc238570344c09d478
Jason Choi <jjc7ru@gmail.com>
2022-06-10 16:23:55 -0400

    optimize code

--- LS175/lesson_05/projects/hello-world/hello.js
+++ LS175/lesson_05/projects/hello-world/hello.js
@@ -2,25 +2,89 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+const COUNTRY_DATA = [
+  {
+    path: "/english",
+    flag: "flag-of-United-States-of-America.png",
+    alt: "US Flag",
+    title: "Go to US English site",
+  },
+  {
+    path: "/french",
+    flag: "flag-of-France.png",
+    alt: "Drapeau de la france",
+    title: "Aller sur le site français",
+  },
+  {
+    path: "/serbian",
+    flag: "flag-of-Serbia.png",
+    alt: "Застава Србије",
+    title: "Идите на српски сајт",
+  },
+];
+
+const LANGUAGE_CODES = {
+  english: "en-US",
+  french: "fr-FR",
+  serbian: "sr-Cryl-rs",
+};
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static('public'));
 app.use(morgan("common"));
 
+app.locals.currentPathClass = (path, currentPath) => {
+  return path === currentPath ? "current" : "";
+};
+
+app.get("/:language", (req, res, next) => {
+  const language = req.params.language;
+  const languageCode = LANGUAGE_CODES[language];
+  if (!languageCode) {
+    //res.status(404).send(`Language not supported: ${language}`);
+    next(new Error(`Language not supported: ${language}`));
+  }
+  res.render(`hello-world-${language}`, {
+    countries: COUNTRY_DATA,
+    currentPath: req.path,
+    lang: languageCode,
+  })
+});
+
+/*
 const showEnglishView = (req, res) => {
-  res.render("hello-world-english");
+  res.render("hello-world-english", {
+    countries: COUNTRY_DATA,
+    currentPath: req.path,
+    lang: 'en-US',
+  });
 }
 app.get("/", showEnglishView);
 app.get("/english", showEnglishView);
 
 app.get("/french", (req, res) => {
-  res.render("hello-world-french");
+  res.render("hello-world-french", {
+    countries: COUNTRY_DATA,
+    currentPath: req.path,
+    lang: 'fr-FR',
+  });
 });
 
 app.get("/serbian", (req, res) => {
-  res.render("hello-world-serbian");
+  res.render("hello-world-serbian", {
+    countries: COUNTRY_DATA,
+    currentPath: req.path,
+    lang: 'sr-Cyrl-rs',
+  });
 });
+*/
+
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
+})
 
 app.listen(3000, 'localhost', () => {
   console.log("Listening to port 3000.");
